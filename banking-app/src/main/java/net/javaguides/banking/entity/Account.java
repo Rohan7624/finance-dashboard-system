46d4d4458d75182456b0e8b8d7229f807bf7bd78
jagadishpradhan <pradhanjagadish76@gmail.com>
2025-06-27 08:48:01 +0530

    banking app code implement

---
+++ banking-app/src/main/java/net/javaguides/banking/entity/Account.java
@@ -0,0 +1,25 @@
+package net.javaguides.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Account")
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @Column(name = "Account_Holder_Name")
+    private String AccountHolderName ;
+    private Double Balance;
+
+}
